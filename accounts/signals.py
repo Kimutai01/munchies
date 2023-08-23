17805d7daa97ab16bbb98ee89e6f7f235d6b020d
Kimutai01 <kiprotichkimutai01@gmail.com>
2023-08-23 10:28:09 +0300

    changes

--- accounts/signals.py
+++ accounts/signals.py
@@ -2,18 +2,18 @@ from django.db.models.signals import post_save, pre_save
 from django.dispatch import receiver
 from .models import User, Profile
 
-@receiver(post_save, sender=User)
-def post_save_create_profile_receiver(sender, instance, created, **kwargs):
-    print(created)
-    if created:
-        Profile.objects.create(user=instance)
-        print("Profile created!")
-    else:
-        try:
-            profile = Profile.objects.get(user=instance)
-            profile.save()
-            print("Profile updated!")
-        except:
-            Profile.objects.create(user=instance)
-            print("Profile created!")
+# @receiver(post_save, sender=User)
+# def post_save_create_profile_receiver(sender, instance, created, **kwargs):
+#     print(created)
+#     if created:
+#         Profile.objects.create(user=instance)
+#         print("Profile created!")
+#     else:
+#         try:
+#             profile = Profile.objects.get(user=instance)
+#             profile.save()
+#             print("Profile updated!")
+#         except:
+#             Profile.objects.create(user=instance)
+#             print("Profile created!")
          
